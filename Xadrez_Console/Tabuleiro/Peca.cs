a4fbcdc29ac339d60ff271b20d0694702d23a0f9
“Milton <miltontenoriodacostas@outlook.com>
2019-06-02 20:39:31 -0300

    Testes iniciais com movimentos possiveis das peças

--- Xadrez_Console/Tabuleiro/Peca.cs
+++ Xadrez_Console/Tabuleiro/Peca.cs
@@ -1,13 +1,13 @@
 ﻿namespace tabuleiro
 {
-    class Peca
+    abstract class Peca
     {
         public Posicao posicao { get; set; }
         public Cor cor { get; protected set; }
         public int qteMovimentos { get; protected set; }
         public Tabuleiro tab { get; protected set; }
 
-        public Peca (Tabuleiro tabuleiro, Cor cor)
+        public Peca (Tabuleiro tab, Cor cor)
         {
             this.posicao = null;
             this.tab = tab;
@@ -19,5 +19,8 @@
         {
             qteMovimentos++;
         }
+
+        // Metodo que define para onde cada peca pode se mover
+        public abstract bool[,] movimentosPossiveis();
     }
 }
